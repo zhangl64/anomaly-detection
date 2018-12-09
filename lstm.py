012e1cc9c446618813ccf437f81ef38fa85a8feb
Lei <huntchang830@gmail.com>
2018-12-09 00:04:16 -0500

    cnn+lstm and lstm+cnn

--- lstm.py
+++ lstm.py
@@ -4,7 +4,6 @@
 # This is an LSTM RNN model for anomaly detection in time series
 # Author: Lei Zhang; Email: leizhang@ryerson.ca
 
-# todo: CNN as the input layer;
 # todo: change the label policy between max and last
 # todo: change the output from 1D to time_steps*D
 
@@ -21,15 +20,20 @@ import itertools
 # create datasets
 # x - time series (timestamp and variables)
 # y - anomaly or normal (boolean)
-def get_data(filename, col_val, col_bool, time_steps=None):
+def get_data(filename, col_val, col_bool, time_steps, split=None):
     dataframe = read_csv(filename, skiprows=0)
     x = dataframe[[col_val]].values
     y = dataframe[[col_bool]].values
 
-    if time_steps is None:
+    if split is not None:
         # reshape x to 3D: samples, time_steps, and features
-        x_reshape = np.reshape(x, (x.shape[0], 1, x.shape[1]))  # change 2D to 3D
+        # x_reshape = np.reshape(x, (x.shape[0], 1, x.shape[1]))  # change 2D to 3D
         # y = np.reshape(y, (y.shape[0], 1, y.shape[1])) # chan  ge 2D to 3D
+        x_reshape = np.reshape(x, (int(x.shape[0] / time_steps), time_steps, x.shape[1]))  # change 2D to 3D
+        y = np.reshape(y, (int(y.shape[0] / time_steps), time_steps))
+        # reduce y to the max value on each row only
+        y = np.amax(y, axis=1)
+        y = np.reshape(y, (y.shape[0], 1, 1))
     else:
         # x = np.reshape(x, (int(x.shape[0]/time_steps), time_steps, x.shape[1]))  # change 2D to 3D
         # reshape x into a 2d array with column_size = time_steps, then reshape x into a 3d array
@@ -40,6 +44,7 @@ def get_data(filename, col_val, col_bool, time_steps=None):
         x_reshape = np.reshape(x_reshape, (x_reshape.shape[0], x_reshape.shape[1], 1))
         # truncate y
         y = y[time_steps-1:]
+        y = np.reshape(y, (y.shape[0], 1, 1))
         # reshape y to 2d, and get the max of each row
         # y = np.reshape(y, (int(y.shape[0]/time_steps), time_steps))
         # y = np.amax(y, axis=1)
@@ -65,8 +70,32 @@ def get_multi_data(fileprefix, col_val, col_bool, start, end):
     return x_all, y_all
 
 
-# build the model
-def build_model(sequence, time_steps, data_dim):
+# build the model of cnn+lstm
+def build_cnn_lstm(sequence, time_steps, data_dim):
+    neuron = int(sequence / time_steps)
+
+    model = Sequential()
+
+    # cnn
+    model.add(Conv1D(filters=sequence, input_shape=(time_steps, data_dim), kernel_size=5, padding='same', activation='relu'))
+    # model.add(GlobalMaxPool1D())
+    model.add(MaxPooling1D(pool_size=time_steps, strides=1))
+
+    # lstm
+    for i in range(2):
+        model.add(LSTM(neuron, dropout=0.2, recurrent_dropout=0.2, return_sequences=True))
+
+    # output layer
+    model.add(Dense(1, activation='linear'))
+    model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae', 'mape'])
+
+    # output shape: 1st dimension - batch size.
+    print(model.summary())
+    return model
+
+
+# build the model of lstm+cnn
+def build_lstm_cnn(sequence, time_steps, data_dim):
     model = Sequential()
 
     # return_sequences must be True for a stack of LSTM layers
@@ -92,7 +121,7 @@ def build_model(sequence, time_steps, data_dim):
 
 # generate sample weights based on y_train and class_weight_dict
 def generate_sample_weights(training_data, class_weight_dictionary):
-    #sample_weights = [class_weight_dictionary[np.where(one_hot_row == 1)[0][0]] for one_hot_row in training_data]
+    # sample_weights = [class_weight_dictionary[np.where(one_hot_row == 1)[0][0]] for one_hot_row in training_data]
     sample_weights = np.zeros(training_data.shape[0])
     for i in range(training_data.shape[0]):
         if training_data[i] == 0:
@@ -190,18 +219,18 @@ def plot_confusion_matrix(cm, classes,
 # main
 def main():
     # create data
-    #x_train, y_train = get_multi_data('../datasets/webscope/A3Benchmark/A3Benchmark-TS', 'value', 'anomaly', 1, 100)
-    #x_test, y_test = get_data('../datasets/webscope/A3Benchmark/A3Benchmark-TS100.csv', 'value', 'anomaly')
+    # x_train, y_train = get_multi_data('../datasets/webscope/A3Benchmark/A3Benchmark-TS', 'value', 'anomaly', 1, 100)
+    # x_test, y_test = get_data('../datasets/webscope/A3Benchmark/A3Benchmark-TS100.csv', 'value', 'anomaly')
     time_steps = 4
     x_train, y_train = get_data('~/Projects/IBM/simulator/mem_train.csv', 'mem', 'label', time_steps)
     x_test, y_test = get_data('~/Projects/IBM/simulator/mem_test.csv', 'mem', 'label', time_steps)
 
     # build the model
-    sequence = 50
+    sequence = 52
     time_steps = x_train.shape[1]
     # time_steps = 10
     data_dim = x_train.shape[2]
-    model = build_model(sequence, time_steps, data_dim)
+    model = build_cnn_lstm(sequence, time_steps, data_dim)
 
     # fit the model
     # class_weight does not support for 3+ dimensional targets
@@ -210,7 +239,7 @@ def main():
     ratio = (y_train.shape[0] - class_1) / class_1
     class_weight_dict = {0: 1, 1: ratio}
 
-    history = model.fit(x_train, y_train, epochs=100, batch_size=5, verbose=2,
+    history = model.fit(x_train, y_train, epochs=150, batch_size=10, verbose=2,
                         sample_weight=generate_sample_weights(y_train, class_weight_dict), validation_split=0.1)
 
     # evaluate the model
@@ -220,7 +249,7 @@ def main():
 
     # predict
     predictions = model.predict(x_test)
-    #rounded = [int(round(x[0])) for x in predictions]
+    # rounded = [int(round(x[0])) for x in predictions]
     rounded = np.round(predictions)
     # reshape
     rounded = np.array(rounded)
@@ -231,6 +260,8 @@ def main():
     pro_plot(x_test, predictions, y_test, rounded, loss_fun, mse_fun)
 
     # plot the confusion matrix
+    y_test = np.reshape(y_test, (y_test.shape[0]))
+    rounded = np.reshape(rounded, (rounded.shape[0]))
     cm = confusion_matrix(y_test, rounded)
     target_names = ['Normal', 'Anomaly']
     plot_confusion_matrix(cm, classes=target_names,
